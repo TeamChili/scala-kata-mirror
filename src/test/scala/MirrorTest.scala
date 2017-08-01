0fc147ec8cb6f36b8d612e7d0220e7e21362f258
Lukasz Konopski <lukasz.konopski.extern@zooplus.com>
2017-08-01 10:39:32 +0200

    vertical tests

--- src/test/scala/MirrorTest.scala
+++ src/test/scala/MirrorTest.scala
@@ -17,5 +17,38 @@ class MirrorTest extends FunSuite {
       assert( result == expected )
     }
 
+  test("should mirror verictally") {
+    val input = "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"
+    val expected = "QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw"
+    val result = Mirror.vertical(input)
+
+    assert( result == expected )
+  }
+
+  test("should mirror verictally 2") {
+    val input = "IzOTWE\nkkbeCM\nWuzZxM\nvDddJw\njiJyHF\nPVHfSx"
+    val expected = "EWTOzI\nMCebkk\nMxZzuW\nwJddDv\nFHyJij\nxSfHVP"
+    val result = Mirror.vertical(input)
+
+    assert( result == expected )
+  }
+
+  test("should mirror verictally 3") {
+    val input = "cuQW\nxOuD\nfZwp\neqFx"
+    val expected = "WQuc\nDuOx\npwZf\nxFqe"
+    val result = Mirror.vertical(input)
+
+    assert( result == expected )
+  }
+
+  test("should mirror verictally even more") {
+    val input = "CDGIdolo\nUstXfrIg\ntMqjvxWL\nBEyuCnAm\nxsxaEERa\nxZsvOiZS\nLutlBRXE\ntxshhbqE"
+    val expected = "olodIGDC\ngIrfXtsU\nLWxvjqMt\nmAnCuyEB\naREEaxsx\nSZiOvsZx\nEXRBltuL\nEqbhhsxt"
+    val result = Mirror.vertical(input)
+
+    assert( result == expected )
+  }
+
+
 
 }
